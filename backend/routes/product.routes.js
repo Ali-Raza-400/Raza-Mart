e9bc05d40dc3f3a7fce9389022836a86df5a3e8b
ali-raza-400 <aliraza.12748g@gmail.com>
2024-11-25 03:17:19 +0500

    test commit

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.route("/add").post(protect,admin,saveProduct);
 router.route("/single/:id").get(protect,getSingleProduct);
 router.route("/product-by-cat/:id").get(protect,getSingleProductByCat);
-router.route("/all").get(protect,getProducts);
+router.route("/all").get(getProducts);
 // router.route("/update").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
